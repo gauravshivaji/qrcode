329a3c15116deab57720eea6e009b399b745d3b2
gauravshivaji <33542486+gauravshivaji@users.noreply.github.com>
2025-11-19 13:17:33 +0530

    Update and rename index.html to app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="My Name", page_icon="👤", layout="centered")
+
+st.title("👤 My Name")
+st.header("Gaurav Bhosale")
+
+st.write("Welcome to my personal QR code website!")
